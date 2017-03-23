7a93f45aebd3d20940d851071f8838e9dd015fc2
abdul.salam <abdul.salam@nttdata.com>
2017-03-23 13:14:59 +0530

    jenkin test

--- app.js
+++ app.js
@@ -8,5 +8,6 @@ app.get('/', function (req, res) {
 
 var port =  (process.env.PORT || 5000) 
 app.listen(port);
-console.log('Server started on port:'+port) 
+console.log('Server started on port:'+port)
+console.log('to stop cntl+c') 
 module.exports = app;
